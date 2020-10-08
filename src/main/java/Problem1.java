a52bcb37ec035205326cae84714cb2e4836f9b25
TrentLucas <itsstatiic@gmail.com>
2020-10-08 16:00:02 -0700

    submission 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,6 +3,14 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        return -1; // Place holder
+        int searchValue = 7; //Value to look for
+        int endValue = -1; //Output
+        int[] list = {1, 3, 6, 8, 9};
+        for (int i = list.length - 1; i > 0; i--) {
+            if (searchValue == list[i]) {
+                endValue = i;
+            }
+        }
+        return endValue; //Return statement
     }
 }
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+
 public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
+        int value1;
+        int value2;
+        for (int j = 0; j < data.length; j++) {
+            for (int i = 0; i < data.length - 1; i++) {
+                value1 = data[i];
+                value2 = data[i + 1];
+                if (value1 > value2) {
+                    data[i] = value2;
+                    data[i+1] = value1;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(data));
     }
 }
